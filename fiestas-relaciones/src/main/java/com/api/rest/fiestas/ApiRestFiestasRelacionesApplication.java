6430ea5320937a8654dea892eed9df551e75566b
Maxi Santucho <maximilianosantucho@outlook.es>
2025-05-08 08:31:51 -0300

    refactor

--- fiestas-relaciones/src/main/java/com/api/rest/fiestas/ApiRestFiestasRelacionesApplication.java
+++ fiestas-relaciones/src/main/java/com/api/rest/fiestas/ApiRestFiestasRelacionesApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ApiRestBibliotecaLibroBidireccionalApplication {
+public class ApiRestFiestasRelacionesApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiRestBibliotecaLibroBidireccionalApplication.class, args);
+		SpringApplication.run(ApiRestFiestasRelacionesApplication.class, args);
 	}
 
 }
